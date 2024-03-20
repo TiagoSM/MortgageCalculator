301095c676d7b456b4594d9a73e4f730feaff52f
TiagoSM <tiagosantosmes@gmail.com>
2024-03-20 19:17:08 -0300

    Initial commit

---
+++ src/estruturalJava/Main.java
@@ -0,0 +1,38 @@
+package estruturalJava;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Principal: ");
+		int principal = scanner.nextInt();
+		System.out.print("Principal: ");
+	}
+}
+	
+	
+	
+	
+	
+	/* Arrays
+	 * public static void main (String[] args) {
+	 
+		int[] numbers = new int[3];
+		numbers[0] = 7;
+		numbers[1] = 3;
+		numbers[2] = 1;
+		
+		for(int i = 0; i<3; i++) {
+			System.out.println(numbers[i]);
+		}
+		
+		Arrays.sort(numbers);
+		
+		for(int i = 0; i<3; i++) {
+			System.out.println(numbers[i]);
+		}
+		
+		String result = Arrays.toString(numbers);
+		System.out.println(result);*/
---
+++ src/mortgagecalculator/Main.java
@@ -0,0 +1,29 @@
+package mortgagecalculator;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args) {
+		final int MONTHSYEAR = 12;
+		final byte PERCENT = 100;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Principal: ");
+		int principal = scanner.nextInt(); // 100000
+		
+		System.out.print("Annual Interest Rate: ");
+		float annualInterestRate = scanner.nextFloat(); // 0,06 / 12
+		float monthlyInterestRate = annualInterestRate / PERCENT / MONTHSYEAR;
+		
+		System.out.print("Period (Years): "); //1
+		int period = scanner.nextInt(); // 15 * 12
+		int n = period * MONTHSYEAR; // 12
+		
+		double mortgage = principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, n)) / (Math.pow(1 + monthlyInterestRate, n) - 1);
+		
+		String mortgageResult = String.format("%.2f", mortgage);
+		System.out.print("Mortgage: " + mortgageResult);
+	}
+}
