0cd3eb3356c332d1951a78f5be40ae92a580f1ab
TiagoSM <tiagosantosmes@gmail.com>
2024-04-06 15:18:24 -0300

    Added problem handling with the Do While for all code blocks

--- src/mortgagecalculator/Main.java
+++ src/mortgagecalculator/Main.java
@@ -1,24 +1,42 @@
 package mortgagecalculator;
 
 import java.util.Scanner;
-// agora preciso fazer o tratamento de erros
+
 public class Main {
 	public static void main (String[] args) {
+		int principal, period, n;
+		float annualInterestRate, monthlyInterestRate;
 		final int MONTHSYEAR = 12;
 		final byte PERCENT = 100;
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("Principal: ");
-		int principal = scanner.nextInt(); // 100000
+		do {
+			System.out.print("Principal: ");
+			principal = scanner.nextInt(); // 100000
+			if (principal < 1000 || principal > 1000000) {
+				System.out.println("Enter a number between $1000 and $1000000!");
+			}
+		}while(principal < 1000 || principal > 1000000);
 		
-		System.out.print("Annual Interest Rate: ");
-		float annualInterestRate = scanner.nextFloat(); // 0,06 / 12
-		float monthlyInterestRate = annualInterestRate / PERCENT / MONTHSYEAR;
+		do {
+			System.out.print("Annual Interest Rate: ");
+			annualInterestRate = scanner.nextFloat(); // 0,06 / 12
+			monthlyInterestRate = annualInterestRate / PERCENT / MONTHSYEAR;
+			if (monthlyInterestRate <= 0 || monthlyInterestRate > 30) {
+				System.out.println("Enter a number greater than 0 and less or equal to 30!");
+			}
+		} while(monthlyInterestRate <= 0 || monthlyInterestRate > 30);
 		
-		System.out.print("Period (Years): "); //1
-		int period = scanner.nextInt(); // 15 * 12
-		int n = period * MONTHSYEAR; // 12
+		
+		do {
+			System.out.print("Period (Years): "); //1
+			period = scanner.nextInt(); // 15 * 12
+			n = period * MONTHSYEAR; // 12]
+			if (period < 1 || period > 30) {
+				System.out.println("Enter a number greater than 1 and less or equal to 30!");
+			}
+		} while(period < 1 || period > 30);
 		
 		double mortgage = principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, n)) / (Math.pow(1 + monthlyInterestRate, n) - 1);
 		
